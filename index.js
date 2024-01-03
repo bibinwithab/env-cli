f5284181d99c49659c782f7cbf6d866c36c2db04
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-01-03 12:19:14 +0530

    update: set and delete funtionality

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-require('dotenv').config();
-const fs = require('fs');
+const { Command } = require("commander");
+require("dotenv").config();
+const fs = require("fs");
 const program = new Command();
 
 program
-  .version('1.0.0')
-  .description('CLI tool for working with environment variables');
+  .version("1.0.0")
+  .description("CLI tool for working with environment variables [env]");
 
 program
-    .command('get <name>')
-    .description('Get the value of a variable')
-    .action((name) => {
-        const value = process.env[name];
-        if (value) {
-            console.log(`${name} = ${value}`);
-        } else {
-            console.log('Environment variable not found');
-        }
-    });
+  .command("get <name>")
+  .description("Get the value of a variable")
+  .action((name) => {
+    const value = process.env[name];
+    if (value) {
+      console.log(`${name} = ${value}`);
+    } else {
+      console.log("Environment variable not found");
+    }
+  });
 
 program
-    .command('set <name> <value>')
-    .description('Set the value of a variable')
-    .action((name, value) => {
-        process.env[name] = value;
-        fs.writeFileSync('.env', `\n${name} = ${value}`, {flag: 'a'})
-        console.log(`Environment variable ${name} set`)
-    });
+  .command("set <name> <value>")
+  .description("Set the value of a variable")
+  .action((name, value) => {
+    
+    const envContent = fs.readFileSync(".env", "utf-8");
+
+    if (envContent.includes(`${name}=`)) {
+      console.error(
+        `Error: Variable '${name}' already exists in the .env file.`
+      );
+    } else {
+      process.env[name] = value;
+      fs.writeFileSync(".env", `\n${name}=${value}`, { flag: "a" });
+      console.log(`${name} = ${value}`);
+    }
+  });
 
 program
-    .command('del <name>')
-    .description('Delete an environment variable')
-    .action((name) => {
-        const value = process.env[name];
-      if (value) {
-        delete value;
-        fs.writeFileSync('.env', `\n${name}=`, { flag: 'a' });
-        console.log(`${name} deleted.`);
-      } else {
-        console.log(`Environment variable ${name} does not exist.`);
-      }
-    });
-  
+  .command("del <name>")
+  .description("Delete an environment variable")
+  .action((name) => {
+    const value = process.env[name];
+
+    if (value !== undefined) {
+
+      delete process.env[name];
+      const envContent = fs.readFileSync('.env', 'utf-8');
+      const updatedEnvContent = envContent
+        .split('\n')
+        .filter(line => !line.startsWith(`${name}=`))
+        .join('\n');
+      fs.writeFileSync('.env', updatedEnvContent);
+      console.log(`${name} deleted.`);
+    } else {
+      console.log(`Environment variable ${name} does not exist.`);
+    }
+  });
+
 
 program.parse(process.argv);
